e5fc98c71992fc72382f277590dc6443537c8859
DemoJameson <demojameson@gmail.com>
2025-10-23 21:17:28 +0800

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine.UI;
+
+namespace Silksong.SwitchLanguageInGame;
+
+[HarmonyPatch]
+[BepInAutoPlugin(id: "com.demojameson.silksong.switchlanguageingame", name: "Switch Language in Game")]
+public partial class Plugin : BaseUnityPlugin {
+    private static ManualLogSource logger = null!;
+    private static ConfigEntry<bool> enableSwitching = null!;
+    private Harmony? harmony;
+    
+    private void Awake() {
+        logger = Logger;
+        enableSwitching = Config.Bind("General", "Switch Language in Game", true, new ConfigDescription("Support language switching in the game"));
+        enableSwitching.SettingChanged += OnEnableSwitchingOnSettingChanged;
+        harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    private void OnDestroy() {
+        harmony?.UnpatchSelf();
+        enableSwitching.SettingChanged -= OnEnableSwitchingOnSettingChanged;
+    }
+
+    private void OnEnableSwitchingOnSettingChanged(object obj, EventArgs eventArgs) {
+        if (!enableSwitching.Value && UIManager._instance) {
+            var description = UIManager._instance.gameOptionsMenuScreen.transform.Find("Content/LanguageSetting/LanguageOption/Description");
+            if (description) {
+                var text = description.GetComponent<Text>();
+                if (text) {
+                    text.enabled = true;
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Selectable), nameof(Selectable.interactable), MethodType.Setter)]
+    [HarmonyPrefix]
+    private static void SelectableSetInteractable(Selectable __instance, ref bool value) {
+        if (enableSwitching.Value && !value && UIManager._instance && __instance == UIManager._instance.languageSetting) {
+            var description = __instance.gameObject.transform.Find("Description");
+            if (description) {
+                var text = description.GetComponent<Text>();
+                if (text) {
+                    text.enabled = false;
+                }
+            }
+
+            value = true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Silksong.SwitchLanguageInGame
+A Hollow Knight: Silksong mod for supporting language switching in the game.
+
+## Source
+GitHub: [https://github.com/DemoJameson/Silksong.ReturnToBench](https://github.com/DemoJameson/Silksong.ReturnToBench)
+
+## To install
+
+### Thunderstore
+It should all be handled for you auto-magically.
+
+### Manual
+First install BepInEx to your Silksong folder,
+(note: this will break how thunderstore does things)
+
+You can find it at
+https://github.com/BepInEx/BepInEx/releases
+latest stable is currently 5.4.23.4
+
+After unzipping, run the game once, so that the BepInEx folder structure generates
+(ie: there's folders in there apart from just `core`)
+
+Then pull this DLL, or folder including the dll in to
+`Hollow Knight Silksong\BepInEx\plugins`
